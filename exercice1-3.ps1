482aaf341c34025200e18f304cc9debce46a182e
AksamDaroussi <akssone619gmail.com>
2024-03-13 10:37:40 +0100

    exercice1-3

---
+++ exercice1-3.ps1
@@ -0,0 +1,16 @@
+﻿#=============================================================================
+#exercice1-3.ps1
+#Daroussi Aksam
+#13/03/2024
+
+#Version 1
+  
+#Exercice 1-2
+
+#Lister les noms, Identifiants et les descriptions des processus "svchost"
+
+#=============================================================================
+
+$process = Get-Process | Where-Object {$_.ProcessName -like"svchost"} 
+$process | Format-Table Id, Name, Description
+
