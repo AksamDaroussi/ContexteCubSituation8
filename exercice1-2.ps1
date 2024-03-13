2ad260967f7720e0697bb28a477b360310c08b4f
AksamDaroussi <akssone619gmail.com>
2024-03-13 10:06:19 +0100

    exercice1-2

---
+++ exercice1-2.ps1
@@ -0,0 +1,15 @@
+﻿#=============================================================================
+#exercice1-2.ps1
+#Daroussi Aksam
+#13/03/2024
+
+#Version 1
+  
+#Exercice 1-2
+
+#Lister les numéros d'identifiants et les noms des processus "svchost"
+
+#=============================================================================
+
+Get-Process | Where-Object {$_.ProcessName -eq "svchost"} | Format-Table -AutoSize
+
