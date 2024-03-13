abd1e630bdb3378bc0a42bf7cf08b769fd3e685c
AksamDaroussi <akssone619gmail.com>
2024-03-13 11:00:22 +0100

    exercice1-4

---
+++ exercice1-4.ps1
@@ -0,0 +1,17 @@
+﻿#=============================================================================
+#exercice1-4.ps1
+#Daroussi Aksam
+#13/03/2024
+
+#Version 3
+  
+#Exercice 1-2
+
+#Demander à l'utilisateur quel processus il désire filtrer pour l'affichage 
+
+#=============================================================================
+
+$processusName = Read-Host "Quel processus désirez-vous afficher ?"
+$process = Get-Process | Where-Object {$_.ProcessName -like "*$processusName*"} 
+$process | Format-Table Id, Name, Description
+
