28d135395cff5016f1e12602340fbb51d86350d1
AksamDaroussi <akssone619gmail.com>
2024-03-20 11:08:04 +0100

    exercice1-9

---
+++ exercice1-9.ps1
@@ -0,0 +1,25 @@
+﻿#=============================================================================
+#exercice1-9.ps1
+#Daroussi Aksam
+#13/03/2024
+
+#Version 4
+  
+#Exercice 1-9
+
+#Parcourir le fichier "services.csv" précédemment exporté pour les services démarrés
+#Afficher en vert les services contenant le mot "system" dans leur nom, en affichant uniquement leur nom et leur statut
+
+#=============================================================================
+function Get-ServicesStatus {
+    $servicesData = Import-Csv -Path "C:\Scripts\services_demarres.csv"
+    $service | Format-Table Name, Status
+    
+    foreach ($service in $servicesData) {
+        if ($service.Name -match "system") {
+            Write-Host -ForegroundColor Green "$($service.Name) - $($service.Status)"
+        }
+    }
+}
+
+Get-ServicesStatus
