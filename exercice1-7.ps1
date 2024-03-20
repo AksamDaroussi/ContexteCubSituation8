270ff79870ec0cb427637ebcdf72457db8a34b31
AksamDaroussi <akssone619gmail.com>
2024-03-20 10:29:42 +0100

    exercice1-7

---
+++ exercice1-7.ps1
@@ -0,0 +1,27 @@
+﻿#=============================================================================
+#exercice1-4.ps1
+#Daroussi Aksam
+#13/03/2024
+
+#Version 4
+  
+#Exercice 1-5
+
+#Lister les services démarrés du système (Nom, Description et Status) et exporter le résultat dans un fichier "service.csv"
+
+#=============================================================================
+function Get-StartedServices {
+    $services = Get-Service | Where-Object {$_.Status -eq "Running"}
+    $services | Format-Table Name, DisplayName, Status
+    $servicesData = foreach ($service in $services) {
+        [PSCustomObject]@{
+            Name = $service.Name
+            Description = $service.DisplayName
+            Status = $service.Status
+        }
+    }
+    $servicesData | Export-Csv -Path "C:\Scripts\service.csv" -NoTypeInformation
+    Write-Host "Liste des services démarrés exportée dans le fichier service.csv"
+}
+
+Get-StartedServices
